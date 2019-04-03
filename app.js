12fdc5b087ff9715ea8718c8dd2cfb9003e64f26
ramdhon <ramadhonimuhammad@gmail.com>
2019-04-03 21:56:19 +0700

    Muhammad-Ramadhoni

--- app.js
+++ app.js
@@ -3,9 +3,11 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
+const cors = require('cors');
 
 const router = require('./router');
 
+app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
